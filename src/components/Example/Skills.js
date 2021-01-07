4795b9537d8c86174c701e6b7378fd74df7babbf
shuibcodes <abdullahshuib124@gmail.com>
2021-01-07 20:02:49 +0000

    skills

---
+++ src/components/Example/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+
+import Title from '../Title/Title';
+
+const Skills = () => {
+  return (
+    <section id="contact">
+      <Container>
+        <Title title="Skills" />
+      </Container>
+    </section>
+  );
+};
+
+export default Skills;
