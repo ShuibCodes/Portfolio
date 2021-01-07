fdaccccf09c60c11d8e14d1bacb8ebb2b2697f1a
shuibcodes <abdullahshuib124@gmail.com>
2021-01-07 20:15:36 +0000

    skillsss

--- src/components/Example/Skills.js
+++ src/components/Example/Skills.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import { Container } from 'react-bootstrap';
-
+import { DiCss3Full } from 'react-icons';
 import Title from '../Title/Title';
 
 const Skills = () => {
   return (
-    <section id="contact">
+    <>
       <Container>
         <Title title="Skills" />
+        <DiCss3Full />
       </Container>
-    </section>
+    </>
   );
 };
 
