d4a13c38b58a6d4d3e47a716c81ad5b37c8591da
shuibcodes <abdullahshuib124@gmail.com>
2021-02-04 07:52:18 +0000

    updating projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -42,7 +42,8 @@ export const projectsData = [
     title: 'Wing-Chun Academy',
     info:
       "A responsive Gatsby website using Contentful CMS and Sass CSS. I'm using Contentful CMS to manage the blog. I used Netlify to deploy and host the site. ",
-    info2: '',
+    info2:
+      'The Contact page also includes a contact form using FormSpree and custom CSS as well as a Map using the Google Maps API.',
     url: 'https://wingchunacademy.netlify.app/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
