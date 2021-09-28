f856d225047af7a741ca4eea221f68646ef89ed8
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2021-09-28 21:12:14 +0100

    edited about me and deleted CV

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -7,7 +8,7 @@ import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -38,30 +39,12 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={100} delay={100} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {paragraphOne ||
-                    "Learning to code has been a difficult and arduous journey, however, I have never done something so rewarding. Now, I feel at a point where I can build multipage websites with my HTML & CSS skills. I have also spent time learning and practicing JavaScript (including working with API's), functional programming and object oriented programming."}
+                  
+                    I am a self-motivated Javascript developer with a good knowledge of ES6, CSS, API’s and React and D3.js and a host of projects to demonstrate that.Most recently I’ve been working with static site generators like Gatsby to build efficient, fast user-interfaces
                 </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    'I am aware this is a fast growing, modern industry. Therefore, I made sure to learn and use frameworks and tools such as React, Gatsby, SASS and most recently, styled components in order to effeciently build websites using the latest tools. '}
-                </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphThree ||
-                    "I am a developer because I'm inspired to be a better me today than I was yesterday. Put simply, I love to learn and these projects below are a great example of that"}
-                </p>
+                
 
-                {resume && (
-                  <span className="d-flex mt-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href={resume}
-                    >
-                      CV
-                    </a>
-                  </span>
-                )}
+             
               </div>
             </Fade>
           </Col>
